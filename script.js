0db1e042c9496e1265e549b54346cd6cdffdc5d2
Lucky Sibuyi <Lucky1sibuyi@gamil.com>
2024-12-05 10:35:13 +0200

    Update

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+
 document.addEventListener("DOMContentLoaded", () => {
   const toggleButton = document.getElementById("toggleButton");
   const chatbotContainer = document.getElementById("chatbotContainer");
